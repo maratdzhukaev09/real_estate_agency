167324cf911b11f579dae9dd747040f60da2fad8
maratdzhukaev09 <60516734+maratdzhukaev09@users.noreply.github.com>
2020-07-21 20:01:00 +0300

    Remove db_index from flat.liked_by, owner.flats

--- property/models.py
+++ property/models.py
@@ -22,7 +22,7 @@ class Flat(models.Model):
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
     new_building = models.NullBooleanField("Новостройка", db_index=True)
-    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул", blank=True, db_index=True)
+    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул", blank=True)
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
@@ -35,7 +35,6 @@ class Owner(models.Model):
 
     flats = models.ManyToManyField("Flat",
                                    blank=True,
-                                   db_index=True,
                                    verbose_name="Квартиры в собственности",
                                    related_name="owners")
 
