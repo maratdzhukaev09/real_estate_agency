2c43bc4f660660c9b250778c6daa8f33040356ac
maratdzhukaev09 <60516734+maratdzhukaev09@users.noreply.github.com>
2020-07-21 20:16:54 +0300

    Fix mistakes

--- property/models.py
+++ property/models.py
@@ -31,7 +31,7 @@ class Flat(models.Model):
 class Owner(models.Model):
     name = models.CharField("ФИО владельца", max_length=200, db_index=True)
     phonenumber = models.CharField("Номер владельца", max_length=20, db_index=True)
-    phone_pure = PhoneNumberField("Нормалиованный номер владельца", null=True, blank=True, db_index=True)
+    phone_pure = PhoneNumberField("Нормализованный номер владельца", null=True, blank=True, db_index=True)
 
     flats = models.ManyToManyField("Flat",
                                    blank=True,
